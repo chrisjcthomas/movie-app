6e5e7c25900951239e2b77636dc6b816e80b70da
Christopher Thomas <cobekgn@gmail.com>
2025-06-05 13:37:28 -0500

    Fix ESLint configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     react(),
     eslint({
-      overrideConfigFile: '.eslintrc.json'
+      overrideConfigFile: '.eslintrc.cjs'
     })
   ],
   resolve: {
