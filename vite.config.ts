6a94a28a855c05c974193e092dd3c5bf89a3d943
Christopher Thomas <cobekgn@gmail.com>
2025-06-05 13:35:45 -0500

    Fix ESLint configuration in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,12 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslint({
+      overrideConfigFile: '.eslintrc.json'
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
